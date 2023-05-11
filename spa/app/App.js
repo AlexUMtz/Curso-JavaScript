5ab6616d5f2b50bbf9f2e7895b71d8192f8010da
Alejandro <alejandro_umartinez@outlook.com>
2023-05-11 15:32:19 -0600

    Finalización de Curso JavaScript

---
+++ spa/app/App.js
@@ -0,0 +1,19 @@
+import api from "./helpers/wp_api.js";
+import { Header } from "./components/Header.js";
+import { Loader } from "./components/loader.js";
+import { Main } from "./components/Main.js";
+import { Router } from "./components/Router.js";
+import { InfiniteScroll } from "./helpers/infinite_scroll.js";
+
+export function App() {
+  const $root = document.getElementById("root");
+
+  $root.innerHTML = null;
+
+  $root.appendChild(Header());
+  $root.appendChild(Main());
+  $root.appendChild(Loader());
+
+  Router();
+  InfiniteScroll();
+}
